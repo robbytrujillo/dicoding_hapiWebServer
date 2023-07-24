6565babf0fd6533ee83dbfd8bbdaa3b11d4db35e
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-07-24 15:35:08 +0700

    Method/Verb Request & Routing

--- routes.js
+++ routes.js
@@ -6,10 +6,24 @@ const routes = [{
     },
 },
 {
+    method: '*',
+    path: '/',
+    handler: (request, h) => {
+        return 'Halaman tidak dapat diakses dengan method tersebut';
+    }
+},
+{
     method : 'GET',
     path: '/about',
     handler: (request, h) => {
         return 'About page';
+    },
+},
+{
+    method: '*',
+    path: '/{any*}',
+    handler: (request, h) => {
+        return 'Halaman tidak ditemukan';
     },
 },
 ];
